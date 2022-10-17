5a6c9fe48dd9dd022cbcae40a3c84945fbfcf60f
FCarter09 <fc_vstate@yahoo.com>
2022-10-17 16:08:29 -0400

    user functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 
 // establish a new link to the GraphQL server at its /graphql endpoint
@@ -17,9 +16,19 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 // instantiate the Apollo Client instance and create the connection to the API endpoint
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
